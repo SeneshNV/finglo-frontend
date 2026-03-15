a8e41a3e55c6a364fa7d6338681a796c9174ee65
SeneshNV <seneshnagodavithana@gmail.com>
2026-03-15 07:07:17 +0530

    <CategoryCard key={category.catId} category={category} />

--- app/category/components/CategoryGrid.tsx
+++ app/category/components/CategoryGrid.tsx
@@ -23,8 +23,8 @@ const CategoryGrid: React.FC<CategoryGridProps> = ({ categories }) => {
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-      {categories.map((category) => (
-        <CategoryCard key={category.catId} category={category} />
+      {categories.map((category, index) => (
+        <CategoryCard key={category.catId} category={category} index={index} />
       ))}
     </div>
   );
